bb3a7f9aeb10e1c930103e9f65e2e9e25cb1dfbb
Marcin Stramowski <marcin.stramowski@netguru.co>
2019-03-17 12:04:53 +0100

    Update

--- app/src/main/java/pl/marcin/gyrophoto/MainActivity.kt
+++ app/src/main/java/pl/marcin/gyrophoto/MainActivity.kt
@@ -4,33 +4,35 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorManager.SENSOR_DELAY_FASTEST
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 import com.github.pwittchen.reactivesensors.library.ReactiveSensorEvent
 import com.github.pwittchen.reactivesensors.library.ReactiveSensorFilter
 import com.github.pwittchen.reactivesensors.library.ReactiveSensors
 import io.fotoapparat.Fotoapparat
-import io.fotoapparat.result.BitmapPhoto
 import io.fotoapparat.result.adapter.rxjava2.toSingle
-import io.fotoapparat.result.transformer.scaled
-import io.reactivex.Flowable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.BiFunction
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fotoapparat: Fotoapparat
 
     private val compositeDisposable = CompositeDisposable()
+    private var lastGyroValues: ReactiveSensorEvent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,52 +42,80 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializeCameraWithPermissions() {
-        val permission = Manifest.permission.CAMERA
-        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED){
+        val cameraPermission = Manifest.permission.CAMERA
+        val writeFilePermission = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        if (checkPermissionGranted(cameraPermission) && checkPermissionGranted(writeFilePermission)) {
             fotoapparat = Fotoapparat(
                 context = this,
                 view = camera_view
             )
         } else {
-            ActivityCompat.requestPermissions(this, arrayOf(permission), 1234)
+            ActivityCompat.requestPermissions(
+                this, arrayOf(cameraPermission, writeFilePermission), 1234
+            )
             finish()
         }
     }
 
-    private fun takePhoto() {
-        compositeDisposable += Single.zip(
-            getPhotoSingle(),
-            getGyroSingle(),
-            BiFunction { photo: BitmapPhoto, gyroValues: ReactiveSensorEvent -> photo to gyroValues }
-        ).subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
+    private fun checkPermissionGranted(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun initGyro() {
+        compositeDisposable += ReactiveSensors(this).observeSensor(
+            Sensor.TYPE_GYROSCOPE,
+            SENSOR_DELAY_FASTEST
+        )
+            .subscribeOn(Schedulers.computation())
+            .filter(ReactiveSensorFilter.filterSensorChanged())
+            .onBackpressureLatest()
             .subscribeBy(
-                onSuccess = { pair ->
-                    val (photoBitmap, gyroValues) = pair
-                    image.setImageBitmap(photoBitmap.bitmap)
-                    image.rotation = -photoBitmap.rotationDegrees.toFloat()
-                    gyrpParams.text = gyroValues.sensorEvent.values.joinToString()
+                onNext = {
+                    lastGyroValues = it
+                    gyroText.text = lastGyroValues?.sensorEvent?.values?.joinToString("\n")
                 },
-                onError = { Log.e("TAG", "Error!", it) }
+                onError = { Log.e("TAG", "Error", it) }
             )
     }
 
-    private fun getGyroSingle(): Single<ReactiveSensorEvent>? {
-        return ReactiveSensors(this).observeSensor(Sensor.TYPE_GYROSCOPE, SENSOR_DELAY_FASTEST)
-            .subscribeOn(Schedulers.computation())
-            .filter(ReactiveSensorFilter.filterSensorChanged())
-            .firstOrError()
+    private fun takePhoto() {
+        compositeDisposable +=
+            getPhotoWithGyroSingle()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onSuccess = { filePath ->
+                        Toast.makeText(this, "Success: $filePath", Toast.LENGTH_SHORT).show()
+                    },
+                    onError = { Log.e("TAG", "Error!", it) }
+                )
     }
 
-    private fun getPhotoSingle(): Single<BitmapPhoto> {
+    private fun getSavePhotoSingle(file: File): Single<Unit> {
         return fotoapparat.takePicture()
-            .toBitmap(scaled(0.2f))
+            .saveToFile(file)
             .toSingle()
+    }
+
+    private fun getPhotoWithGyroSingle(): Single<String> {
+        val file = createFile(lastGyroValues)
+        return getSavePhotoSingle(file).ignoreElement().toSingleDefault(file.absolutePath)
+    }
+
+    private fun createFile(gyroEvent: ReactiveSensorEvent?): File {
+        val fileName = "${Date()}_${gyroEvent?.sensorEvent?.values?.joinToString("_")}.jpg"
+        val file = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+            fileName
+        )
+        Log.e("TAG", file.absolutePath.toString())
+        return file
     }
 
     override fun onStart() {
         super.onStart()
         fotoapparat.start()
+        initGyro()
     }
 
     override fun onStop() {
